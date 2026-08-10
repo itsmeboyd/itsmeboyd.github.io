c6ca7bdfac670ff6bf8bf45e8a829af00a7e433f
Virgilio Delfin Jr <vadjr27@gmail.com>
2026-08-11 00:32:07 +0800

    refactor: remove unused getTechStackIcon function and related imports for cleaner code

--- src/lib/utils.jsx
+++ src/lib/utils.jsx
@@ -1,18 +1,5 @@
-import { TechStackData } from "../data/TechStackData";
-
 export const handleHashChange = (setCurrentHash) => {
   return () => {
     setCurrentHash(window.location.hash || "#home");
   };
-};
-
-export const getTechStackIcon = (techName) => {
-  const iconName = TechStackData.find((tech) => tech.name === techName)?.icon;
-  console.log(
-    "getTechStackIcon called with techName:",
-    techName,
-    "found icon:",
-    iconName,
-  );
-  // return iconName || "";
 };
